2c14f9365eb77138b5679a1065223422340a98da
Jiyun <brightnk@outlook.com>
2017-06-27 19:51:36 -0700

    ready to develop term project v1

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import config from '../config';
+
+
+//set router using express moudle.
+const router = express.Router();
+
+export default router;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	entry: './src/index.js',
+	output:{
+		//input index.js, and bundle all moudule depended into bundle.js
+		path: __dirname + '/static',
+		filename: 'bundle.js'
+	},
+	module:{
+		loaders: [
+			{
+				//every file end with js, run babel-loader on it
+				//babel-loader will transforming all none standard js liek JSX, ES2015
+				test: /\.js$/,
+				loader: 'babel-loader'
+			},
+			{
+				//npm i -S json-loader, so webpack and read Json file
+				test: /\.json$/,
+				loader:'json-loader'
+
+			}
+
+		]
+	}
+};
